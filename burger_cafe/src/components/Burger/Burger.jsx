7bb0b2048fec03e135a1a5e24a59741bcb26e8e2
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-17 15:05:18 +0300

    drynamic burger

--- burger_cafe/src/components/Burger/Burger.jsx
+++ burger_cafe/src/components/Burger/Burger.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import styles from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = () => {
+const burger = (props) => {
+    // Object.keys is used to convert the object to Array. it takes the keys into an
+    // array
+    const transformIngredients = Object
+        .keys(props.ingredients)
+        .map(ingrKey => {
+            return [...Array(props.ingredients[ingrKey])].map((_,i)=> {
+                return <BurgerIngredient key={ingrKey + i} type={ingrKey} />
+            });
+        });
+
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformIngredients}
             <BurgerIngredient type="bread-bottom"/>
 
         </div>
--- burger_cafe/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ burger_cafe/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -3,12 +3,28 @@ import Auxi from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //   super(props);
+    
+    //   this.state = {
+         
+    //   }
+    // }
+
+    state={
+        ingredients : {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+    
+
     render() {
         return (
             <Auxi>
-                <div>Burger</div>
-                <div>Build Controls</div>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
             </Auxi>
         );
     }
