6e6596929480bc0f7c1db40b5e7fb14eaf532fab
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-17 16:15:57 +0300

    dynamic burger. converting object to array and nested map. also for loop

--- burger_cafe/src/components/Burger/Burger.jsx
+++ burger_cafe/src/components/Burger/Burger.jsx
@@ -5,18 +5,30 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
     // Object.keys is used to convert the object to Array. it takes the keys into an
     // array
-    const transformIngredients = Object
-        .keys(props.ingredients)
-        .map(ingrKey => {
-            return [...Array(props.ingredients[ingrKey])].map((_,i)=> {
-                return <BurgerIngredient key={ingrKey + i} type={ingrKey} />
-            });
+    const ingKeysArray = Object.keys(props.ingredients);
+    const transformIngredients = ingKeysArray
+    .map(ingrKey => {
+        return [...Array(props.ingredients[ingrKey])].map((_, i) => {
+            console.log(i)
+            return <BurgerIngredient key={ingrKey + i} type={ingrKey}/>
         });
+    });
+
+    // alternative
+    // const keys = Object.keys(props.ingredients);
+    // let transformIngredients = [];
+    // for ( let i = 0; i < keys.length; i++ ) {
+    //      let ingredientCount = props.ingredients[keys[i]];
+        
+    //     for ( let j=0; j < ingredientCount; j++ ) {
+    //         transformIngredients.push(<BurgerIngredient key={keys[i] + j} type={keys[i]} />);
+    //     };
+    // }
+
 
     return (
         <div className={styles.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            {transformIngredients}
+            <BurgerIngredient type="bread-top"/> {transformIngredients}
             <BurgerIngredient type="bread-bottom"/>
 
         </div>
--- burger_cafe/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ burger_cafe/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -13,10 +13,10 @@ class BurgerBuilder extends Component {
 
     state={
         ingredients : {
-            salad: 1,
+            salad: 2,
             bacon: 1,
             cheese: 2,
-            meat: 2
+            meat: 2,
         }
     }
     
