e4408a00344f0c8fea96e0f6553980c26775f15b
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-18 16:50:31 +0300

    backdrop set up but not working yet

--- burger_cafe/src/components/UI/Modal/Modal.jsx
+++ burger_cafe/src/components/UI/Modal/Modal.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from './Modal.css';
+import Auxi from '../../../hoc/Auxi';
+import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <div
-        className={styles.Modal}
-        style={{
-        transform: props.show
-            ? 'translateY(0)'
-            : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-    }}>
-        {props.children}
-    </div>
-)
+const modal = (props) => {
+    return (
+        <Auxi>
+            <Backdrop show={props.show}/>
+            <div
+                className={styles.Modal}
+                style={{
+                transform: props.show
+                    ? 'translateY(0)'
+                    : 'translateY(-100vh)',
+                opacity: props.show
+                    ? '1'
+                    : '0'
+            }}>
+                {props.children}
+            </div>
+        </Auxi>
 
+    )
+}
 export default modal;
