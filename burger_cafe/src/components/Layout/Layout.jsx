57d3058a519e5ae1960778dad0fc3396508a0878
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-05-19 14:46:33 +0300

    nav link

--- burger_cafe/src/components/Layout/Layout.jsx
+++ burger_cafe/src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Auxi from '../../hoc/Auxi';
 import styles from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Auxi>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={styles.Content}>{props.children}</main>
     </Auxi>
 );
---
+++ burger_cafe/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './NavigationItem.css'
+
+const navigationItem = (props) => {
+    return (
+        <div>
+            <li className={styles.NavigationItem}>
+                <a href={props.link}
+                className={props.active ? styles.active : null}
+                >{props.children}</a>
+            </li>
+        </div>
+    )
+}
+
+export default navigationItem;
