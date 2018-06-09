c4f801b98c1a7f08cc5de831f99e925b8978ae07
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-09 11:05:53 +0300

    order of ingredients changed

--- burger_cafe/src/containers/Checkout/Checkout.jsx
+++ burger_cafe/src/containers/Checkout/Checkout.jsx
@@ -31,9 +31,9 @@ class Checkout extends Component {
 
                 <Route
                     path={this.props.match.path + '/contact-data'}
-                    component={ContactData} />)}/>
+                    component={ContactData} />
             </div>
-        )
+        );
     }
 }
 
