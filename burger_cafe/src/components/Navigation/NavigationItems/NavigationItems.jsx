1833d4e8ddec3064c1870ef8e63db840280bcb58
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-10 22:55:30 +0300

    Adding an authentication form

--- burger_cafe/src/components/Navigation/NavigationItems/NavigationItems.jsx
+++ burger_cafe/src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -6,6 +6,7 @@ const navigationItems = () => (
     <ul className={styles.NavigationItems}>
         <NavigationItem link="/" exact>Burger Maker </NavigationItem>
         <NavigationItem link="/orders">Orders </NavigationItem>
+        <NavigationItem link="/auth">Authenticate </NavigationItem>
     </ul>
 )
 
