a8188bbad81ea7aae126de806391211038a852e6
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-20 14:20:43 +0300

    handling auto sign-in with Saga

--- burger_cafe/src/store/sagas/auth.js
+++ burger_cafe/src/store/sagas/auth.js
@@ -21,28 +21,47 @@ export function* checkAuthTimeoutSaga(action) {
 }
 
 export function* authUserSaga(action) {
-        yield put(actions.authStart())
-        const authData = {
-            email: action.email,
-            password: action.password,
-            returnSecureToken: true
-        };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBsHOObWVoOX_XUlx7oQGsfcBsQ9B4y68I';
-        if (!action.isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBsHOObWVoOX_XUlx7oQGsfcBsQ9B4y68I'
-        }
+    yield put(actions.authStart())
+    const authData = {
+        email: action.email,
+        password: action.password,
+        returnSecureToken: true
+    };
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBsHOObWVoOX_XUlx7oQGsfcBsQ9B4y68I';
+    if (!action.isSignup) {
+        url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBsHOObWVoOX_XUlx7oQGsfcBsQ9B4y68I'
+    }
 
-        try {
-            const response = yield axios.post(url, authData)
+    try {
+        const response = yield axios.post(url, authData)
 
-            // console.log(response);
-            const expirationDate = yield new Date(new Date().getTime() + response.data.expiresIn * 1000);
-            yield localStorage.setItem('token', response.data.idToken);
-            yield localStorage.setItem('expirationDate', expirationDate);
-            yield localStorage.setItem('userId', response.data.localId);
-            yield put(actions.authSuccess(response.data.idToken, response.data.localId));
-            yield put(actions.checkAuthTimeout(response.data.expiresIn));
-        } catch (error) {
-            yield put(actions.authFail(error.response.data.error))
+        // console.log(response);
+        const expirationDate = yield new Date(new Date().getTime() + response.data.expiresIn * 1000);
+        yield localStorage.setItem('token', response.data.idToken);
+        yield localStorage.setItem('expirationDate', expirationDate);
+        yield localStorage.setItem('userId', response.data.localId);
+        yield put(actions.authSuccess(response.data.idToken, response.data.localId));
+        yield put(actions.checkAuthTimeout(response.data.expiresIn));
+    } catch (error) {
+        yield put(actions.authFail(error.response.data.error))
+    }
+}
+
+
+export function* authCheckStateSaga() {
+    const token = yield localStorage.getItem('token');
+    if (!token) {
+        yield put(actions.logout());
+    } else {
+        const expirationDate = yield new Date(localStorage.getItem('expirationDate'));
+        if (expirationDate <= new Date()) {
+            yield put(actions.logout());
+        } else {
+            const userId = localStorage.getItem('userId')
+            yield put(actions.authSuccess(token, userId));
+            yield put(actions.checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+
+
         }
     }
+}
