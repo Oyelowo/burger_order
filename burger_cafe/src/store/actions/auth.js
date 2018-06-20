4ca1ccdee30726f0268a87ed82a52cab2c544e85
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-06-20 13:03:48 +0300

    moving logic into saga

--- burger_cafe/src/store/actions/auth.js
+++ burger_cafe/src/store/actions/auth.js
@@ -31,13 +31,23 @@ export const logout = () => {
     };
 };
 
+export const logoutSucceed = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
 
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000);
+    return {
+        type: actionTypes.AUTH_CHECK_TIMEOUT,
+        expirationTime: expirationTime
     };
+
+    // return dispatch => {
+    //     setTimeout(() => {
+    //         dispatch(logout());
+    //     }, expirationTime * 1000);
+    // };
 };
 
 
